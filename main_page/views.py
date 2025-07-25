cedfef3a3fff71dd568dc36156202d3589152815
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-07-25 04:54:06 +0300

    fix part 2 ex-8

--- main_page/views.py
+++ main_page/views.py
@@ -17,17 +17,20 @@ class MainView(ListView):
     def get_context_data(self, **kwargs):
         """ Отображение рассылок """
         context = super().get_context_data(**kwargs)
-        unsuccessful_mailings = 0
-        if Newsletter.objects.filter(status="Запущена").count():
-            unsuccessful_mailings += 1
+        # Пропишем переменные для обращения к модели рассылок
+        completed_count = Newsletter.objects.filter(status="Завершена").count()
+        active_count = Newsletter.objects.filter(status="Запущена").count()
+        total_count = Newsletter.objects.count()
+        unsuccessful_mailings = Newsletter.objects.exclude(status="Завершена").count()
+
         context.update(
             {
-                'item_count': Newsletter.objects.count(), # количество всех рассылок
-                'active_count': Newsletter.objects.filter(status="Запущена").count(), # количество активных рассылок
+                'item_count': total_count, # количество всех рассылок
+                'active_count': active_count, # количество активных рассылок
                 'unique_clients': Recipient.objects.distinct().count(), # количество уникальных получателей
-                'successful_mailings': Newsletter.objects.filter(status="Запущена").count(),  # успешные попытки рассылок
-                'unsuccessful_mailings': Newsletter.objects.filter(status="Создана").count(),  # неуспешные попытки рассылок
-                'sent_messages': unsuccessful_mailings,  # количество отправленных сообщений
+                'successful_mailings': completed_count,  # успешные попытки рассылок
+                'unsuccessful_mailings': unsuccessful_mailings,  # неуспешные попытки рассылок
+                'sent_messages': completed_count,  # количество отправленных сообщений
             }
         )
         return context
